e2c4a65c5f53c0c222204e8a504fd46a0fd38252
Tal <talzemah1@gmail.com>
2018-02-07 14:07:21 +0200

    Commit for sync

--- ComputerVision/Source.cpp
+++ ComputerVision/Source.cpp
@@ -7,32 +7,49 @@ using namespace cv;
 int main()
 {
 	// Source video
-	string filename = "source.mp4";
+	string filename = "input/source.mp4";
 	VideoCapture capture(filename);
 
 	if (!capture.isOpened())
+	{
 		cout << "Error when reading the video file..";
+		return 1;
+	}
 
 	String windowName = "Panorama";
 	vector<Mat> framesArr;
 	Mat tempFrame;
 	Mat panorama;
+	const int NUM_OF_FRAMES = 5;
+	const int STRIP_WIDTH = 400; /// change to percent
+	//const int STRIP_HEIGHT = 20;
 
-	int numOfFrames = capture.get(CV_CAP_PROP_FRAME_COUNT);
+	int frameCount = capture.get(CV_CAP_PROP_FRAME_COUNT);
+	int width = capture.get(CV_CAP_PROP_FRAME_WIDTH);
+	int height = capture.get(CV_CAP_PROP_FRAME_HEIGHT) - 1;
 
 	// Get frames from source video.
 	for (int i = 0; ; i++)
 	{
 		capture >> tempFrame;
-
 		if (tempFrame.empty())
 			break;
 
-		if (i % (numOfFrames / 8) == 0)
+		if (i % (frameCount / NUM_OF_FRAMES) == 0)
 			framesArr.push_back(tempFrame.clone());
 
 		//imwrite(to_string(i / 50) + ".jpg", frame.clone());
 	}
+
+	//// Select strip from each frame.
+	//for (int i = 0; i < framesArr.size(); i++)
+	//{
+	//	tempFrame = framesArr[i].clone();
+	//	Rect stripSize(Point((width - STRIP_WIDTH) / 2, 0), Point((width + STRIP_WIDTH) / 2, height));
+	//	tempFrame = tempFrame(stripSize);
+	//	framesArr[i] = tempFrame;
+	//}
+
 
 	// Stitching all frames into one panorama.
 	Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA, true);
@@ -44,6 +61,7 @@ int main()
 		return -1;
 	}
 
+	//destroyAllWindows();
 	//namedWindow(windowName, WINDOW_KEEPRATIO);
 	imshow(windowName, panorama);
 	imwrite("pano.jpg", panorama.clone());
---
+++ ComputerVision/Test.cpp
@@ -0,0 +1,98 @@
+//#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/highgui/highgui.hpp"
+//#include <opencv2\stitching.hpp>
+//#include <vector>
+//#include <iostream>
+//
+////int RECT_X = 844;
+////int RECT_Y = 477;
+//
+//using namespace std;
+//using namespace cv;
+//
+//Mat pixelToTransparency(cv::Mat input, int r, int g, int b);
+//
+////Rect rect(Point(0, 0), Point(RECT_X, RECT_Y));
+////Rect rect1(Point(0, 0), Point(RECT_X*0.7, RECT_Y));
+////Rect rect2(Point(0, 0), Point(RECT_X, RECT_Y));
+////Rect rect3(Point(RECT_X*0.3, 0), Point(RECT_X, RECT_Y));
+//
+//int main()
+//{
+//	VideoCapture video("source.mp4"); // video source
+//
+//	if (!video.isOpened())
+//	{
+//		puts("***Could not initialize capturing...***\n");
+//		return 1;
+//	}
+//
+//	int size = (int)video.get(CAP_PROP_FRAME_COUNT);
+//
+//	//int RECT_X = 844;
+//	//int RECT_Y = 477;
+//	int RECT_X = (int)video.get(CAP_PROP_FRAME_WIDTH);
+//	int RECT_Y = (int)video.get(CAP_PROP_FRAME_HEIGHT);
+//
+//	Rect rect(Point(0, 0), Point(RECT_X, RECT_Y));
+//	Rect rect1(Point(0, 0), Point(RECT_X*0.7, RECT_Y));
+//	Rect rect2(Point(0, 0), Point(RECT_X, RECT_Y));
+//	Rect rect3(Point(RECT_X*0.3, 0), Point(RECT_X, RECT_Y));
+//
+//	Mat frame, copy_frame;
+//	vector<Mat> leftFrames, rightFrames;
+//	for (int i = 0; i < size; i++)
+//	{
+//		video >> frame;
+//		copy_frame = frame.clone();
+//
+//		frame = frame(rect);
+//		frame = frame(rect1);
+//		if (i % 40 == 0)
+//			leftFrames.push_back(frame);
+//
+//		copy_frame = copy_frame(rect2);
+//		copy_frame = copy_frame(rect3);
+//		if (i % 40 == 0)
+//			rightFrames.push_back(copy_frame);
+//
+//		char c = (char)waitKey(int(video.get(CV_CAP_PROP_FPS)));
+//		if (c == 27)
+//			break;
+//	}
+//
+//	destroyAllWindows();
+//	frame.release();
+//
+//	Mat leftEyeResult, rightEyeResult, theFrames;
+//
+//	hconcat(leftEyeResult, theFrames);
+//	imwrite("theFramesL.jpg", theFrames);
+//	Stitcher s1 = Stitcher::createDefault();
+//	s1.stitch(leftFrames, leftEyeResult);
+//	imwrite("leftEyeResult.jpg", leftEyeResult);
+//	cv::Mat leftEye = leftEyeResult; // load as color image BGR
+//	imwrite("Left Eye After Transparency.png", pixelToTransparency(leftEye, 0, 0, 0)); // save as .png file (which supports alpha channels/transparency)
+//
+//	hconcat(rightEyeResult, theFrames);
+//	imwrite("theFramesR.jpg", theFrames);
+//	Stitcher s2 = Stitcher::createDefault();
+//	s2.stitch(rightFrames, rightEyeResult);
+//	imwrite("rightEyeResult.jpg", rightEyeResult);
+//	cv::Mat rightEye = rightEyeResult; // load as color image BGR
+//	imwrite("Right Eye After Transparency.png", pixelToTransparency(rightEye, 0, 0, 0)); // save as .png file (which supports alpha channels/transparency)
+//}
+//
+//cv::Mat pixelToTransparency(cv::Mat input, int r, int g, int b)
+//{
+//	cv::Mat input_bgra2;
+//	cv::cvtColor(input, input_bgra2, CV_BGR2BGRA);
+//	for (int y = 0; y < input_bgra2.rows; ++y)
+//		for (int x = 0; x < input_bgra2.cols; ++x)
+//		{
+//			cv::Vec4b & pixel = input_bgra2.at<cv::Vec4b>(y, x);
+//			if (pixel[0] == r && pixel[1] == g && pixel[2] == b)
+//				pixel[3] = 0;
+//		}
+//	return input_bgra2;
+//}
