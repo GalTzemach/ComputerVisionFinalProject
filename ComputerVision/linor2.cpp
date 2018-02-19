556c225166fedad0681f922b2b5d7ad72012a254
Tal <talzemah1@gmail.com>
2018-02-19 12:45:44 +0200

    BackUp 02

--- ComputerVision/linor2.cpp
+++ ComputerVision/linor2.cpp
@@ -25,15 +25,15 @@ int main()
 	Mat leftPano;
 	Mat rightPano;
 
-	const int NUM_OF_FRAMES = 10;
-	//const int STRIP_WIDTH = 350; /// change to percent
+	const int NUM_OF_FRAMES = 20;
+	const double STRIP_WIDTH = 0.6; // Percent
 
 	int frameCount = video.get(CV_CAP_PROP_FRAME_COUNT);
 	int width = video.get(CV_CAP_PROP_FRAME_WIDTH);
 	int height = video.get(CV_CAP_PROP_FRAME_HEIGHT) - 1;
 
-	Rect rectLeft(Point(0, 0), Point(width*0.7, height));
-	Rect rectRight(Point(width*0.3, 0), Point(width, height));
+	Rect rectLeft(Point(0, 0), Point(width * STRIP_WIDTH, height));
+	Rect rectRight(Point(width * (1 - STRIP_WIDTH), 0), Point(width, height));
 
 	for (int i = 0; i < frameCount; i++)
 	{
@@ -47,10 +47,6 @@ int main()
 			rightArr.push_back(tempFrame.clone());
 			rightArr.back() = rightArr.back()(rectRight);
 		}
-
-		/*char c = (char)waitKey(int(video.get(CV_CAP_PROP_FPS)));
-		if (c == 27)
-			break;*/
 	}
 
 	//tempFrame.release();
@@ -74,8 +70,8 @@ int main()
 		return -1;
 	}
 
-	imwrite("output/linor/leftPano.jpg", leftPano);
-	imwrite("output/linor/rightPano.jpg", rightPano);
+	imwrite("output/winter/leftPano.jpg", leftPano);
+	imwrite("output/winter/rightPano.jpg", rightPano);
 
 	endTime = clock();
 	cout << "Total time = " << (double)(endTime - startTime) / CLOCKS_PER_SEC << " Sec" << endl;
