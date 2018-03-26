085afdaa21694a641ef36ae6e56f9a3c3cf0382e
Tal <talzemah1@gmail.com>
2018-03-27 01:40:51 +0300

    BackUp.

--- ComputerVision/setStripsFromSides2.cpp
+++ ComputerVision/setStripsFromSides2.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 int main()
 {
 	// My var.
-	const double STRIP_WIDTH = 0.6;
+	const double STRIP_WIDTH = 0.99;
 	const int frameSamplingRate = 8;
 
 	const int startFrame = 115;
@@ -35,7 +35,9 @@ int main()
 
 	// For hold the relevant frame/s.
 	vector<Mat> leftFrameArr, rightFrameArr;
-	Mat tempFrame, leftPano, rightPano;
+	Mat tempFrame;
+	Mat leftPano, leftPano1, leftPano2;
+	Mat rightPano, rightPano1, rightPano2;
 
 	// Get info from source video.
 	int frameCount = video.get(CV_CAP_PROP_FRAME_COUNT);
@@ -65,31 +67,169 @@ int main()
 	cout << "Number of frame = " << leftFrameArr.size() << endl;
 	cout << "Strip size = " << STRIP_WIDTH << endl;
 
+	// Divide to two parts.
+	vector<Mat> leftFrameArr1, leftFrameArr2;
+	vector<Mat> rightFrameArr1, rightFrameArr2;
+
+	for (int i = 0; i < leftFrameArr.size(); i++)
+	{
+		if (i < leftFrameArr.size() / 2)
+		{
+			leftFrameArr1.push_back(leftFrameArr[i]);
+
+			rightFrameArr1.push_back(rightFrameArr[i]);
+		}
+		else if (i == leftFrameArr.size() / 2)
+		{
+			leftFrameArr1.push_back(leftFrameArr[i]);
+			leftFrameArr2.push_back(leftFrameArr[i]);
+
+			rightFrameArr1.push_back(rightFrameArr[i]);
+			rightFrameArr2.push_back(rightFrameArr[i]);
+		}
+		else
+		{
+			leftFrameArr2.push_back(leftFrameArr[i]);
+
+			rightFrameArr2.push_back(rightFrameArr[i]);
+		}
+	}
+
 	// Stitching all frames into one panorama.
 	Ptr<Stitcher> stitcher = Stitcher::create(mode, try_use_gpu);
 	Stitcher::Status status;
 
+	// Left panorama.
 	cout << "Stitching left panorama..." << endl;
 
+	// Part 1.
+	cout << "estimateTransform (part 1)..." << endl;
+	status = stitcher->estimateTransform(leftFrameArr1);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't estimateTransform in leftFrameArr1 (part 1), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	cout << "composePanorama (part 1)..." << endl;
+	status = stitcher->composePanorama(leftFrameArr1, leftPano1);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't composePanorama in leftFrameArr1 (part 1), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	// Part 2.
+	cout << "estimateTransform (part 2)..." << endl;
+	status = stitcher->estimateTransform(leftFrameArr2);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't estimateTransform in leftFrameArr2 (part 2), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	cout << "composePanorama (part 2)..." << endl;
+	status = stitcher->composePanorama(leftFrameArr2, leftPano2);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't composePanorama in leftFrameArr2 (part 2), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	// Final left.
+	leftFrameArr = { leftPano1, leftPano2 };
+
+	cout << "estimateTransform (final pano)..." << endl;
 	status = stitcher->estimateTransform(leftFrameArr);
 	if (status != Stitcher::OK)
 	{
-		cout << "Can't estimateTransform in leftFrameArr, error code = " << int(status) << endl;
+		cout << "Can't estimateTransform in leftFrameArr (final pano), error code = " << int(status) << endl;
 		system("pause");
 		return -1;
 	}
 
+	cout << "composePanorama (final pano)..." << endl;
 	status = stitcher->composePanorama(leftFrameArr, leftPano);
 	if (status != Stitcher::OK)
 	{
-		cout << "Can't composePanorama in leftFrameArr, error code = " << int(status) << endl;
+		cout << "Can't composePanorama in leftFrameArr (final pano), error code = " << int(status) << endl;
 		system("pause");
 		return -1;
 	}
 
-	// Save results to computer.
+	// Save left results to computer.
 	imwrite("output/leftPano.jpg", leftPano);
-	//imwrite("output/rightPano.jpg", rightPano);
+
+
+
+	// Right panorama.
+	cout << "Stitching right panorama..." << endl;
+
+	// Part 1.
+	cout << "estimateTransform (part 1)..." << endl;
+	status = stitcher->estimateTransform(rightFrameArr1);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't estimateTransform in rightFrameArr1 (part 1), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	cout << "composePanorama (part 1)..." << endl;
+	status = stitcher->composePanorama(rightFrameArr1, rightPano1);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't composePanorama in rightFrameArr1 (part 1), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	// Part 2.
+	cout << "estimateTransform (part 2)..." << endl;
+	status = stitcher->estimateTransform(rightFrameArr2);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't estimateTransform in rightFrameArr2 (part 2), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	cout << "composePanorama (part 2)..." << endl;
+	status = stitcher->composePanorama(rightFrameArr2, rightPano);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't composePanorama in rightFrameArr2 (part 2), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	// Final right.
+	leftFrameArr = { rightPano1, rightPano2 };
+
+	cout << "estimateTransform (final pano)..." << endl;
+	status = stitcher->estimateTransform(rightFrameArr);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't estimateTransform in rightFrameArr (final pano), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	cout << "composePanorama (final pano)..." << endl;
+	status = stitcher->composePanorama(rightFrameArr, rightPano);
+	if (status != Stitcher::OK)
+	{
+		cout << "Can't composePanorama in rightFrameArr (final pano), error code = " << int(status) << endl;
+		system("pause");
+		return -1;
+	}
+
+	// Save right results to computer.
+	imwrite("output/rightPano.jpg", rightPano);
 
 	// Print time elapsed
 	endTime = clock();
